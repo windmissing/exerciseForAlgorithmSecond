098d39b6e62f97303efe27f56e2464512a5f77d7
yinahe <yina.he@nokia.com>
2015-10-22 16:03:01 +0800

    add cases for exercise 22.3-6 upload gitignore

--- tst/chapter22/Exercise22_3_6Test.cpp
+++ tst/chapter22/Exercise22_3_6Test.cpp
@@ -1,24 +1,9 @@
 #include "gtest/gtest.h"
 
 using namespace std;
-#include "Exercise22_3_6.h"
 #include <vector>
-/*
-q s
-s v
-v w
-w s
-q w
-q t
-t y
-y q
-t x
-x z
-z x
-u y
-r y
-r u
-*/
+
+#include "Exercise22_3_6.h"
 
 #define     NUMOF(x)        (sizeof(x)/sizeof(*(x)))
 
@@ -26,6 +11,8 @@ class G_for_test : public Graph
 {
 public:
 	void inputTestData(vector<int> v);
+	void testVisitTime(int vId, int start, int end);
+	void testEdgeType(int *type, int len);
 };
 
 vector<int> data2Vector(char *data, int len)
@@ -38,7 +25,7 @@ vector<int> data2Vector(char *data, int len)
 
 void G_for_test::inputTestData(vector<int> v)
 {
-	for(int i = 1; i <= v.size(); i+=2)
+	for(int i = 0; i < v.size(); i+=2)
 	{
 		Edge *E = new Edge(v[i], v[i+1]);
 		InsertEdge(this, E);
@@ -48,6 +35,31 @@ void G_for_test::inputTestData(vector<int> v)
 	}
 }
 
+void G_for_test::testVisitTime(int vId, int start, int end)
+{
+	EXPECT_EQ(start, V[vId]->d);
+	EXPECT_EQ(end, V[vId]->f);
+}
+
+/*
+ * 1:"树边" 2:"反向边" 3:"正向边" 4:"交叉边"
+ * */
+void G_for_test::testEdgeType(int *type, int len)
+{
+	int cnt = 0;
+	for(int i = 1; i <= N; i++)
+	{
+		Edge *e = V[i]->head;
+		while(e)
+		{
+			EXPECT_EQ(type[cnt], e->type) << e->start << ' '<< e->end <<' '<<e->type;
+			cnt++;
+			if(cnt >= len)break;
+			e = e->next;
+		}
+		if(cnt >= len)break;
+	}
+}
 TEST(data_in_book, data_in_book)
 {
 	char data[] = 
@@ -70,59 +82,19 @@ TEST(data_in_book, data_in_book)
 	vector<int> v = data2Vector(data, NUMOF(data));
 	G_for_test *G = new G_for_test();
 	G->inputTestData(v);
-	//DFS(G);
-}
-#if 0
-int main()
-{
-	//构造一个空的图
-	Graph *G = new Graph;
-	Print(G);
-	//输入边
-	int i;
-	char start, end;
-	for(i = 1; i <= 14; i++)
-	{
-		cin>>start>>end;
-		E = new Edge(start-'p', end-'p');
-		InsertEdge(G, E);
-		//无向图，要加两条边
-//		E = new Edge(end, start);
-//		InsertEdge(G, E);
-	}
-	Print(G);
-	//深度优先搜索
 	DFS(G);
-	//输出每个顶点的第一次访问时间和访问结束的时间
-	for(i = 1; i <= N; i++)
-	{
-		cout<<char('p'+i)<<':';
-		cout<<G->V[i]->d<<' '<<G->V[i]->f<<endl;
-	}
-	for(i = 1; i <= N; i++)
-	{
-		Edge *e = G->V[i]->head;
-		while(e)
-		{
-			cout<<char(e->start+'p')<<"->"<<char(e->end+'p')<<": ";
-			switch (e->type)
-			{
-			case 1:
-				cout<<"树边"<<endl;
-				break;
-			case 2:
-				cout<<"反向边"<<endl;
-				break;
-			case 3:
-				cout<<"正向边"<<endl;
-				break;
-			case 4:
-				cout<<"交叉边"<<endl;
-				break;
-			}
-			e = e->next;
-		}
-	}
-	return 0;
+	G->testVisitTime(1, 1, 16);
+	G->testVisitTime(2, 17, 20);
+	G->testVisitTime(3, 2, 7);
+	G->testVisitTime(4, 8, 15);
+	G->testVisitTime(5, 18, 19);
+	G->testVisitTime(6, 3, 6);
+	G->testVisitTime(7, 4, 5);
+	G->testVisitTime(8, 9, 12);
+	G->testVisitTime(9, 13, 14);
+	G->testVisitTime(10, 10, 11);
+
+	int edgeType[] = {1, 1, 3, 1, 4, 1, 1, 1, 4, 1, 2, 1, 2, 2};
+	G->testEdgeType(edgeType, NUMOF(edgeType));
+	delete G;
 }
-#endif
