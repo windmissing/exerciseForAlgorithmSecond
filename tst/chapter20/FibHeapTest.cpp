86086f8a71cc4c7bf6516b5f5a49fe712424d4d0
yinahe <yina.he@nokia.com>
2015-10-26 18:43:03 +0800

    add some UT cases for fib heap

--- tst/chapter20/FibHeapTest.cpp
+++ tst/chapter20/FibHeapTest.cpp
@@ -8,3 +8,42 @@
 #include<iostream>
 using namespace std;
 
+#include "gtest/gtest.h"
+#include "FibHeap.h"
+
+TEST(initFibHeap, currentSizeShouldBe0)
+{
+	CFibHeap H;
+	EXPECT_EQ(0, H.getDataCount());
+}
+
+TEST(getTopData, oneDataShouldGetTheData)
+{
+	CFibHeap H;
+	node *pN = new node(3);
+	EXPECT_EQ(SUCCESS, H.insert(pN));
+	node *pTop = H.extract();
+	EXPECT_EQ(3, pTop->key);
+	delete pTop;
+}
+
+TEST(merge, merge)
+{
+	CFibHeap H1;
+	node *pN1 = new node(5);
+	H1.insert(pN1);
+
+	CFibHeap H2;
+	node *pN2 = new node(3);
+	H2.insert(pN2);
+
+	EXPECT_EQ(SUCCESS, H1.merge(&H2));
+
+	node *pTop = H1.extract();
+	EXPECT_EQ(3, pTop->key);
+	delete pTop;
+
+	pTop = H1.extract();
+	EXPECT_EQ(5, pTop->key);
+	delete pTop;
+}
